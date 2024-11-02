b6a7067beb91c03938377c550855207cf3c20ea5
Payal Kumari <146120927+Payal8736383@users.noreply.github.com>
2024-11-02 17:17:05 +0000

    datatype

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 12345
+let accountEmail = "payalsinghrajput981@gamil.com"
+var accountPassword = 123456789
+accountCity = "bihar"
+let accountStates;
+
+// accountId = 2  NOT ALLOWED
+accountEmail="payalchauhan981@gmail.com"
+accountPassword="987654321"
+accountCity="delhi"
+// console.log(accountId);
+console.log([accountId , accountEmail , accountPassword , accountCity , accountStates]);
+
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all javaScript code as newer version
+
+//alert(3+3)// We are using node js , Not browser.... we can do later there is some different syntex in node jd
+let name = "payal"
+let age = 22
+let areYou18pluse = true
+
+
+//PRIMITIVE DATA-TYPE //
+//--------------------------------------------------------------------------------------------------------//
+//number=> 2 to the power 53
+//bigint=> it is use for big data
+//string
+//boolean=> true or false
+//null=> null is a value , null is not undifine value in node js it is a empty value
+//undifined=> like when u defined variable but u didnot declear value 
+//symbol=> it is use for uniqueness
+
+
+//NONPRIMITIVE DATA-TYPE//
+//OBJECT
+
+
+console.log(typeof "payal")
