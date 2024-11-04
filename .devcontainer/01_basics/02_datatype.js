45d1043c9e7029feeb64a713d1693fcb5cbfbd52
Payal Kumari <146120927+Payal8736383@users.noreply.github.com>
2024-11-04 16:07:05 +0000

    datatype variables

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict";//treat all javaScript code as newer version   (Syntax should be proper)
+
+//alert(3+3) We are using node js , Not browser.... we can do later there is some different syntex in node jd
+let name = "payal"
+let course = 'mca'
+let age = `${name} is 22 ${course} ` //Template string(Backticks)
+
+
+//PRIMITIVE DATA-TYPE 
+//number=> 2 to the power 53
+//bigint=> it is use for big data
+//string
+//boolean=> true or false
+//null=> null is a value , null is not undifine value in node js it is a empty value
+//undifined=> like when u defined variable but u didnot declear value 
+//symbol=> it is use for uniqueness
+
+
+//NONPRIMITIVE DATA-TYPE
+//OBJECT
+console.log(age)
+
+console.log(typeof "payal");
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof symbol)
